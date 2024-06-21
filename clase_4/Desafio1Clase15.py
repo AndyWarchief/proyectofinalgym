abf2c18ee5a823a454f1a668853d44e152a58a64
AndyWarchief <andyzepol25f@gmail.com>
2024-06-21 19:43:25 -0300

    Delete clase_4 directory

--- clase_4/Desafio1Clase15.py
+++
@@ -1,7 +0,0 @@
-nombre1=input("ingrese un nombre: ")
-nombre2=input("ingrese un segundo nombre: ")
-if nombre1 == nombre2 :
-    print("ambas cadenas son iguales!")
-else:
-    print("las cadenas son diferentes")
-print("adios!")
