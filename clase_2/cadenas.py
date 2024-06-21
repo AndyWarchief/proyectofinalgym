ce865d1bd71bd6c3b0e2c2f3a2d37430f8fcc15d
AndyWarchief <andyzepol25f@gmail.com>
2024-06-21 19:43:09 -0300

    Delete clase_2 directory

--- clase_2/cadenas.py
+++
@@ -1,7 +0,0 @@
-class Perro:
-    def __init__(self,nombre,raza):
-        self.nombre=nombre
-        self.raza=raza
-    def ladrar(self):
-        print("guau guau")
-        miperro=Perro("Firulais")
--- clase_2/variables.py
+++
@@ -1,5 +0,0 @@
-nom="Juan"
-print(type(nom))
-edad=25
-print(type(edad))
-print("nombre: " + nom + "su edad es: " , edad)
